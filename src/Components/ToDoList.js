cc8d8211845ea6f9d4780bf11e1e6a8ce2034a0e
Josipa Dadić <josipa.dadic@gmail.com>
2020-09-23 13:00:17 +0200

    completed bug fixed

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
 
-export default function ToDoList({
-  todoitems,
-  deleteHandler,
-  completedHandler,
-}) {
+export default function ToDoList({ todoitems, onDelete, onCompleted }) {
   return todoitems.map((todo) => {
     return (
       <ToDoItem
         key={todo.id}
         todo={todo}
-        onDelete={deleteHandler}
-        onCompleted={completedHandler}
+        onDelete={onDelete}
+        onCompleted={onCompleted}
       />
     );
   });
